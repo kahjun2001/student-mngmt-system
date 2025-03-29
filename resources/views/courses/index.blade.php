d064de00b5a637c28e9e73c8e0a6023e6dc6a02b
Lee Kah Jun <kahjun1018@gmail.com>
2025-03-30 05:34:31 +0800

    feat: style sidebar using Tailwind CSS for improved layout and design

---
+++ resources/views/courses/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>This is the course tab</h1>
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,3 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Exports</h1>
-    <a href="{{ url('/export/student-averages') }}">Download Student Averages</a>
-    <br>
-    <a href="{{ url('/export/subject-averages') }}">Download Subject Averages</a>
-</body>
-</html> --}}
-
 @extends('layouts.app')
 
 @section('content')
